78042d9937612c3e16cda5a4fbb17bc2c72d2b2e
AleksYLeciejewski <babsel123@gmail.com>
2024-12-16 10:36:23 +0100

    Rettet manglende klasser, der ikke blev kopieret

--- src/Items/Armor/ChestArmor.java
+++ src/Items/Armor/ChestArmor.java
@@ -1,4 +1,17 @@
 package Items.Armor;
+import Items.Item;
+import Items.ItemType;
 
-public class ChestArmor {
+public class ChestArmor extends Item implements ItemType {
+    public String name;
+
+    public ChestArmor(int itemID, String name, double weight, int maxStack, boolean isStackable, double defense){
+        super(name, itemID, weight, isStackable, maxStack);
+
+    }
+
+    @Override
+    public String getItemType(){
+        return "Chest Armor";
+    }
 }
