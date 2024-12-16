9e5088c0e100af195537555cdc031d42eb0a42ac
AleksYLeciejewski <babsel123@gmail.com>
2024-12-16 10:41:10 +0100

    hej

--- src/Items/Armor/ChestArmor.java
+++ src/Items/Armor/ChestArmor.java
@@ -5,8 +5,8 @@ import Items.ItemType;
 public class ChestArmor extends Item implements ItemType {
     public String name;
 
-    public ChestArmor(int itemID, String name, double weight, int maxStack, boolean isStackable, double defense){
-        super(name, itemID, weight, isStackable, maxStack);
+    public ChestArmor(int itemID, String name, double weight, int maxStack, double defense, int itemType){
+        super(name, itemID, weight, maxStack, itemType);
 
     }
 
