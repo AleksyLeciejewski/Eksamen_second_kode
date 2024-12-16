e60a22daa829365ab1095f98b0f1d8215a11ee68
AleksYLeciejewski <babsel123@gmail.com>
2024-12-16 10:22:41 +0100

    Weapon rettet

--- src/Items/Consumable/Potion.java
+++ src/Items/Consumable/Potion.java
@@ -1,4 +1,21 @@
 package Items.Consumable;
 
-public class Potion {
+public class Potion extends Consumable {
+    public Potion(String name, int itemID, double weight, boolean isStackable, int maxStack, String effect, int duration) {
+        super(name, itemID, weight, isStackable, maxStack, effect, duration);
+    }
+
+    public void applyEffect(){
+        System.out.println("Applying effect: " + this.getEffect() + " for " + this.getDuration() + " seconds");
+    }
+
+    public void drink(){
+        System.out.println("Drinking potion: " + this.getName());
+        applyEffect();
+    }
+
+    public static Potion createHealthPotion(String name, int itemID, int duration){
+        return new Potion("Healing Items.Potion", itemID, 0.5, true, 4, "Healing", 5);
+    }
+
 }
